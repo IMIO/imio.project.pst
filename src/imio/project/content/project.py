fd1173cf8bfe9d605e8f2bd3ee3e67a703131213
Gauthier Bastien <g.bastien@imio.be>
2013-06-05 14:38:50 +0000

    Do not heritate from ITask, we change to much things in the resulting heritated Interface, it ease things...

--- src/imio/project/content/project.py
+++ src/imio/project/content/project.py
@@ -1,17 +1,21 @@
 import datetime
+
 from zope import schema
 from zope.interface import implements
 from zope.interface import Interface
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
+
 from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
 from plone.autoform import directives
 from plone.dexterity.content import Container
 from plone.dexterity.schema import DexteritySchemaPolicy
 from plone.formwidget.datetime.z3cform.widget import DateFieldWidget
-from collective.task.content.task import ITask
+from plone.supermodel import model
+
 from collective.z3cform.datagridfield import DataGridFieldFactory, DictRow
 from collective.z3cform.rolefield.field import LocalRolesToPrincipals
+
 from imio.project import _
 
 
@@ -25,7 +29,7 @@ class IResultIndicatorSchema(Interface):
         required=True,)
 
 
-class IProject(ITask):
+class IProject(model.Schema):
     """
         Project schema, field ordering
     """
@@ -36,6 +40,39 @@ class IProject(ITask):
         required=False,
         value_type=schema.Choice(
             vocabulary=u'imio.project.content.project.category_vocabulary',
+        ),
+    )
+
+    priority = schema.Choice(
+        title=_(u'Priority'),
+        description=_(u"Choose a priority."),
+        vocabulary=u'imio.project.content.project.priority_vocabulary',
+    )
+
+    budget = schema.Text(
+        title=_(u"Budget"),
+        description=_("Budget details"),
+        required=False,
+    )
+
+    responsible = LocalRolesToPrincipals(
+        title=_(u"Responsible"),
+        roles_to_assign=('Editor',),
+        value_type=schema.Choice(
+            vocabulary="plone.principalsource.Principals"
+        ),
+        min_length=1,
+        max_length=1,
+        required=True,
+    )
+
+    visible_for = LocalRolesToPrincipals(
+        title=_(u"Visible for"),
+        description=_(u"Choose principals that can see this project."),
+        required=False,
+        roles_to_assign=('Reader',),
+        value_type=schema.Choice(
+            vocabulary=u'plone.principalsource.Principals'
         ),
     )
 
@@ -50,35 +87,37 @@ class IProject(ITask):
     )
     directives.widget(result_indicator=DataGridFieldFactory)
 
-    priority = schema.Choice(
-        title=_(u'Priority'),
-        description=_(u"Choose a priority."),
-        vocabulary=u'imio.project.content.project.priority_vocabulary',
-    )
-
-    visible_for = LocalRolesToPrincipals(
-        title=_(u"Visible for"),
-        description=_(u"Choose principals that can see this project."),
-        required=False,
-        roles_to_assign=('Reader',),
-        value_type=schema.Choice(
-            vocabulary=u'plone.principalsource.Principals'
-        ),
-    )
-
-    budget = schema.Text(
-        title=_(u"Budget"),
-        description=_("Budget details"),
-        required=False,
-    )
-
     planned_begin_date = schema.Date(
         title=_(u'Planned begin date'),
-        description=_(u"Enter a planned date for the beginning."),
+        description=_(u"Enter the planned begin date."),
         required=False,
         defaultFactory=datetime.date.today,
     )
     directives.widget(planned_begin_date=DateFieldWidget)
+
+    effective_begin_date = schema.Date(
+        title=_(u'Effective begin date'),
+        description=_(u"Enter the effective begin date."),
+        required=False,
+        defaultFactory=datetime.date.today,
+    )
+    directives.widget(effective_begin_date=DateFieldWidget)
+
+    planned_end_date = schema.Date(
+        title=_(u'Planned end date'),
+        description=_(u"Enter the planned end date."),
+        required=False,
+        defaultFactory=datetime.date.today,
+    )
+    directives.widget(planned_end_date=DateFieldWidget)
+
+    effective_end_date = schema.Date(
+        title=_(u'Effective end date'),
+        description=_(u"Enter the effective end date."),
+        required=False,
+        defaultFactory=datetime.date.today,
+    )
+    directives.widget(effective_end_date=DateFieldWidget)
 
     progress = schema.Int(
         title=_(u"Progress"),
@@ -92,10 +131,6 @@ class IProject(ITask):
         required=False,
     )
     directives.widget(comments=WysiwygFieldWidget)
-
-    # remove title coming from ITask as we add it using the dublincore behavior
-    directives.omitted('title')
-    directives.widget(deadline=DateFieldWidget)
 
 
 class Project(Container):
