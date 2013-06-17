56266df5f6f4d1e9a43b7b0fbd3ddc3da29fda6f
Gauthier Bastien <g.bastien@imio.be>
2013-06-17 09:23:41 +0000

    Removed monkey.py, moved to imio.project.core

--- src/imio/project/pst/__init__.py
+++ src/imio/project/pst/__init__.py
@@ -8,6 +8,3 @@ _ = MessageFactory('imio.project.pst')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
-
-    from imio.project.pst import monkey
-    monkey.__name__  # make pyflakes happy...
