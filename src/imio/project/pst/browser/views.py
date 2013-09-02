9e10c96053c76451424162bf6df1f585604b881c
Gauthier Bastien <g.bastien@imio.be>
2013-09-02 13:42:10 +0000

    Moved some base functionnalities to imio.project.core

--- src/imio/project/pst/browser/views.py
+++ src/imio/project/pst/browser/views.py
@@ -10,7 +10,6 @@ from Products.Five import BrowserView
 from Products.statusmessages.interfaces import IStatusMessage
 from plone.app.textfield import RichTextValue
 
-from imio.project.pst.interfaces import IListContainedDexterityObjectsForDisplay
 from imio.project.pst import _
 
 
@@ -25,24 +24,6 @@ def getOsTempFolder():
     else:
         raise "Sorry, I can't find a temp folder on your machine."
     return res
-
-
-class ContainerFolderListingView(BrowserView):
-    """
-      This manage the elements listed on the view of a dexterity container
-    """
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-        portal_state = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
-        self.portal = portal_state.portal()
-
-    def listRenderedContainedElements(self, portal_types=[]):
-        """
-          Get the contained elements, rendered for display
-          If p_portal_types is specified, only return elements having the required portal_type
-        """
-        return IListContainedDexterityObjectsForDisplay(self.context).listContainedObjects()
 
 
 class DocumentGenerationView(BrowserView):
