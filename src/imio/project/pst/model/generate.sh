94e2f099da5c5b1ccda04671e380d229025690cc
Stephan Geulette <s.geulette@imio.be>
2013-09-16 13:56:02 +0000

    Changed transition name (#6518)

--- src/imio/project/pst/model/generate.sh
+++ src/imio/project/pst/model/generate.sh
@@ -8,4 +8,4 @@ cp -r temp/profiles/default/workflows ../profiles/default
 
 #change the workflow name
 sed -i 's/title=\"pst_objective_workflow\"/title=\"PST objective workflow\"/g' ../profiles/default/workflows/pst_objective_workflow/definition.xml
-
+sed -i 's/title=\"pst_action_workflow\"/title=\"PST action workflow\"/g' ../profiles/default/workflows/pst_action_workflow/definition.xml
