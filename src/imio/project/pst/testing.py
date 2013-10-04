ed85fb0ceaabef33b7caccd93fc30f89b60647e9
Stephan Geulette <s.geulette@imio.be>
2013-10-04 12:02:06 +0000

    Added tests (#6024)

--- src/imio/project/pst/testing.py
+++ src/imio/project/pst/testing.py
@@ -5,14 +5,16 @@ import unittest2
 from plone.app.testing import PloneWithPackageLayer
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
-from plone.app.testing import login
+from plone.app.testing import login, logout
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import TEST_USER_NAME
+from datetime import datetime
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
 from plone.dexterity.utils import createContentInContainer
-
+from collective.contact.plonegroup.config import ORGANIZATIONS_REGISTRY, PLONEGROUP_ORG
 import imio.project.pst
-
 
 PST_TESTING_PROFILE = PloneWithPackageLayer(
     zcml_filename="testing.zcml",
@@ -36,6 +38,7 @@ class IntegrationTestCase(unittest2.TestCase):
     def setUp(self):
         super(IntegrationTestCase, self).setUp()
         self.portal = self.layer['portal']
+        self.pst = self.portal['pst']
 
 
 class FunctionalTestCase(unittest2.TestCase):
@@ -46,6 +49,101 @@ class FunctionalTestCase(unittest2.TestCase):
     def setUp(self):
         super(FunctionalTestCase, self).setUp()
         self.portal = self.layer['portal']
-        # login as Manager and add the pst projectspace
+
+
+class BaseTestCase(object):
+    """
+        Base class containing helpers method
+    """
+
+    def login(self, username):
+        login(self.portal, username)
+
+    def addUsers(self):
+        self.portal = self.layer['portal']
+        # login as Manager
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         login(self.portal, TEST_USER_NAME)
+        # add users
+        self.portal.portal_registration.addMember(id="psteditor", password="psteditor")
+        self.portal.acl_users.source_groups.addPrincipalToGroup("psteditor", "pst_editors")
+        # add contacts, plone groups, users
+        self.portal.contacts.invokeFactory('organization', id=PLONEGROUP_ORG)
+        own_orga = self.portal.contacts['plonegroup-organization']
+        createContentInContainer(own_orga, 'organization', **{'title': u'Services'})
+        self.services = self.portal['contacts'][PLONEGROUP_ORG]['services']
+        registry = getUtility(IRegistry)
+        for service in (u'Personnel', u'Compta'):
+            obj = createContentInContainer(own_orga['services'], 'organization', **{'title': service})
+            registry[ORGANIZATIONS_REGISTRY] = registry[ORGANIZATIONS_REGISTRY] + [obj.UID()]
+            user = service.lower()
+            self.portal.portal_registration.addMember(id=user, password=user)
+            self.portal.acl_users.source_groups.addPrincipalToGroup(user, "%s_actioneditor" % obj.UID())
+        logout()
+
+    def addObjects(self):
+        self.portal = self.layer['portal']
+        # login as Manager
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        login(self.portal, TEST_USER_NAME)
+        services = self.portal.contacts['plonegroup-organization']['services']
+        data = {
+            'os1':
+            {
+            'title': u'OS1',
+            'categories': u'volet-externe-dvp-politiques-proprete-securite-publique',
+            'budget': [],
+            'budget_comments': u'',
+            'operationalobjectives': [
+                {
+                    'title': u'OO1-1',
+                    u'result_indicator': [{'value': 50, 'label': u'Nombre de sacs poubelles récoltés '
+                                          u'chaque année et à l\'échéance (31 12 2015)',
+                                          'reached_value': 0}],
+                    'priority': u'1',
+                    'planned_end_date': datetime.date(datetime(2015, 12, 31)),
+                    'representative_responsible': [],
+                    'administrative_responsible': [services['personnel'].UID()],
+                    'manager': ['%s_actioneditor' % services['personnel'].UID()],
+                    'extra_concerned_people': u'Police\r\nAgents constatateurs communaux\r\nAgent ',
+                    'budget': [],
+                    'budget_comments': u'Fonds propres (en cours de chiffrage)',
+                    'comments': u'',
+                    'actions': [
+                        {'title': u'A1-1-1',
+                         'manager': ['%s_actioneditor' % services['personnel'].UID()],
+                         'planned_end_date': datetime.date(datetime(2014, 06, 30)),
+                         'extra_concerned_people': u'La firme adjudicatrice au terme du marché public',
+                         'budget': [{'amount': 12500.0, 'budget_type': 'wallonie', 'year': '2013'},
+                                    ],
+                         'budget_comments': u'1000 euros\r\nBudget ordinaire\r\nArticle budgétaire n°: ...',
+                         'health_indicator': u'risque',
+                         'health_indicator_details': u'Agent traitant malade pour minimum 3 mois -> risque de retard',
+                         'work_plan': u'<p>Les principales tâches à réaliser dans un ordre logique sont:<p>'
+                                      u'<ul>'
+                                      u'<li>inventaire des parcs existants sur la commune (Maxime/Patrick)</li>'
+                                      u'<li>passation d\'un marché public pour commander les distributeurs (Michèle) '
+                                      u'finalisé pour le 01 09 2013</li>'
+                                      u'<li>réception des distributeurs à l\'excution du marché (Michèle)</li>'
+                                      u'<li>placement des distributeurs (Maxime/Patrick) pour le 01 12 2013</li>'
+                                      u'<li>gestion des stocks de sachets (Michèle)</li>'
+                                      u'<li>réapprovisionnement (Maxime)</li>'
+                                      u'</ul>',
+                         'comments': u'Attendre le placement des nouvelles poubelles (avant le 01 12 2013)'
+                         },
+                    ]
+                },
+            ],
+            },
+        }
+
+        # create all this in a folder named 'pst' at the root of the Plone Site
+        pst = self.portal.pst
+        for strategicobjective in data:
+            strategicObj = createContentInContainer(pst, "strategicobjective", id=strategicobjective,
+                                                    **data[strategicobjective])
+            for operationalobjective in data[strategicobjective]['operationalobjectives']:
+                operationalObj = createContentInContainer(strategicObj, "operationalobjective", **operationalobjective)
+                for action in operationalobjective['actions']:
+                    createContentInContainer(operationalObj, "pstaction", **action)
+        logout()
--- src/imio/project/pst/tests/test_action.py
+++ src/imio/project/pst/tests/test_action.py
@@ -1,13 +1,36 @@
 # -*- coding: utf-8 -*-
 """ Content action tests for this package."""
 
-from imio.project.pst.testing import FunctionalTestCase
+from imio.project.pst.testing import IntegrationTestCase, BaseTestCase
+from imio.project.pst.content import action
 
 
-class TestAction(FunctionalTestCase):
+class TestAction(IntegrationTestCase, BaseTestCase):
     """Test installation of imio.project.pst into Plone."""
 
     def setUp(self):
         """Custom shared utility setup for tests."""
         super(TestAction, self).setUp()
+        self.addUsers()
+        self.addObjects()
 
+    def test_default_manager(self):
+        """
+            test default values
+        """
+        oo = self.pst['os1']['oo1-1']
+        act = oo['a1-1-1']
+
+        class Dummy(object):
+            def __init__(self, context):
+                self.context = context
+
+        # we login as a pst editor
+        self.login('psteditor')
+        self.assertEquals(action.default_manager(Dummy(oo)), [])
+        # we login as a service member
+        self.login('personnel')
+        self.assertEquals(action.default_manager(Dummy(oo)), ['%s_actioneditor' % self.services['personnel'].UID()])
+        # we login as a service member, the context isn't the good one
+        self.login('personnel')
+        self.assertEquals(action.default_manager(Dummy(act)), [])
