88e1ec506bcbddd8864e1d323f25f80338f12a6b
Franck Ngaha <franck.o.ngaha@gmail.com>
2015-04-13 10:00:35 +0000

    Fix the view

--- src/imio/project/pst/setuphandlers.py
+++ src/imio/project/pst/setuphandlers.py
@@ -246,7 +246,7 @@ def _createCollections(context):
         ]
         OSs.query = query
         OSs.sort_on = u'reference_number'
-        OSs.setLayout('folder_tabular_view')
+        OSs.setLayout('tabular_view')
         pst.default_page = "objectifs-strategiques"
         site.portal_types.projectspace.filter_content_types = True
         behaviour.setConstrainTypesMode(1)
@@ -299,7 +299,7 @@ def _createStatesCollections(context, container, portal_type):
                 collection.query = query
                 #collection.limit = 10
                 collection.sort_on = u'reference_number'
-                collection.setLayout('folder_tabular_view')
+                collection.setLayout('tabular_view')
 
 
 def _addPSTGroups(context):
