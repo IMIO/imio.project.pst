5c9a9b39f60a102a2e10d65379da181a5a259a4d
cmessiant <cmessiant@localhost>
2016-07-06 12:28:01 +0000

    Configure dashboard in 0.4 migration (ref #11763)

--- src/imio/project/pst/setuphandlers.py
+++ src/imio/project/pst/setuphandlers.py
@@ -856,7 +856,7 @@ COLUMNS_FOR_CONTENT_TYPES = {
         u'select_row',
         u'pretty_link',
         u'review_state',
-        u'CreationDate',
+        u'categories',
         u'history_actions'
     ),
     'operationalobjective': (
