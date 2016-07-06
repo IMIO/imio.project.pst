6b26d27c34c24f1c9c4364f391e30f9fdf61e380
cmessiant <cmessiant@localhost>
2016-07-06 15:16:11 +0000

    Fix HistoryActionsColumn header

--- src/imio/project/pst/columns.py
+++ src/imio/project/pst/columns.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 """Custom columns."""
+from collective.eeafaceted.z3ctable import _ as _cez
+
 from imio.dashboard.columns import ActionsColumn
 
 
 class HistoryActionsColumn(ActionsColumn):
 
+    header = _cez("header_actions")
     header_js = '<script type="text/javascript">jQuery(document).ready(initializeOverlays);' \
                 'jQuery(document).ready(preventDefaultClickTransition);</script>'
     params = {'showHistory': True, 'showActions': False}
