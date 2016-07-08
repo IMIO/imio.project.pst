8323bf2678240da26d59250259cb14b7968d836c
cmessiant <cmessiant@localhost>
2016-07-08 20:31:50 +0000

    Add actions panel views for the 3 content types to sort transitions in actions column (ref #12281)

---
+++ src/imio/project/pst/browser/actionspanel.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from imio.actionspanel.browser.views import ActionsPanelView
+
+
+class SortTransitionsActionsPanelView(ActionsPanelView):
+
+    transitions = []
+
+    def sortTransitions(self, lst):
+        """ Sort transitions following transitions list order"""
+        tr_order = dict((val, i) for (i, val) in enumerate(self.transitions))
+        import pdb; pdb.set_trace()
+        lst.sort(lambda x, y: cmp(tr_order[x['id']], tr_order[y['id']]))
+
+
+class PSTActionsPanelView(SortTransitionsActionsPanelView):
+
+    transitions = [
+        'begin',
+        'set_to_be_scheduled',
+        'stop',
+        'finish',
+        'back_to_created',
+        'back_to_ongoing',
+        'back_to_be_scheduled',
+    ]
+
+
+class ObjectivesActionsPanelView(SortTransitionsActionsPanelView):
+
+    transitions = [
+        'begin',
+        'back_to_created',
+        'achieve',
+        'back_to_ongoing',
+    ]
