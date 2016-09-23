22109482c57a614b1993a03acdda6d692bfb14cc
Stephan Geulette <s.geulette@imio.be>
2016-09-23 09:45:21 +0000

    Temporary class needed to access the site before migration

--- src/imio/project/pst/content/operational.py
+++ src/imio/project/pst/content/operational.py
@@ -3,6 +3,7 @@
 from zope.interface import implements
 from zope import schema
 from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleVocabulary
 
 from plone.autoform import directives as form
 from plone.dexterity.schema import DexteritySchemaPolicy
@@ -91,3 +92,11 @@ class OperationalObjectiveSchemaPolicy(DexteritySchemaPolicy):
 
     def bases(self, schemaName, tree):
         return (IOperationalObjective, )
+
+
+class ManagerVocabulary(object):
+    """ Temporary class needed to access the site before migration. Can be removed after 1.0 """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        return SimpleVocabulary([])
