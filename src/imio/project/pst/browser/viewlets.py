7a6242adc422e5f2490aa76417ecc16195b3682c
Stephan Geulette <s.geulette@imio.be>
2016-11-08 15:38:42 +0000

    Correctly overrides viewlet

--- src/imio/project/pst/browser/viewlets.py
+++ src/imio/project/pst/browser/viewlets.py
@@ -8,6 +8,10 @@ class TasksListViewlet(OriginalTasksListViewlet):
 
     """Tasks list for current task container object."""
 
+    def update(self):
+        if self.context.portal_type in ('task', ):
+            super(TasksListViewlet, self).update()
+
     def render(self):
         if self.context.portal_type in ('task', ):
             return super(TasksListViewlet, self).render()
