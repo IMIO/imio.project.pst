e4caf3539be46a9ab2310afd8d5887b53cf62a3a
Stephan Geulette <s.geulette@imio.be>
2016-11-09 14:29:52 +0000

    Add review_state column

--- src/imio/project/pst/setuphandlers.py
+++ src/imio/project/pst/setuphandlers.py
@@ -587,7 +587,7 @@ COLUMNS_FOR_CONTENT_TYPES = {
     'pstaction': (u'select_row', u'pretty_link', u'review_state', u'manager', u'planned_begin_date',
                   u'planned_end_date', u'effective_begin_date', u'effective_end_date', u'progress',
                   u'health_indicator', u'history_actions'),
-    'task': (u'select_row', u'pretty_link', u'task_parent', u'assigned_group',
+    'task': (u'select_row', u'pretty_link', u'task_parent', u'review_state', u'assigned_group',
              u'assigned_user', u'due_date', u'CreationDate', u'history_actions'),
 }
 
