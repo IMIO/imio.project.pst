9f4a86db66b8073a5f222c874aca4a814d6e31c4
Stephan Geulette <s.geulette@imio.be>
2016-12-08 14:50:40 +0000

    Preparing release 1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.4 (unreleased)
+1.0 (2016-12-08)
 ----------------
 
 - Added dashboard
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='imio.project.pst',
-    version='0.3.1dev',
+    version='1.0',
     description="PST management",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
