d5b16dd759fe02dc0130bc5f7a1f435723eb31a7
Stephan Geulette <s.geulette@imio.be>
2017-02-10 14:26:45 +0000

    Added step to update templates

--- src/imio/project/pst/__init__.py
+++ src/imio/project/pst/__init__.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
 
+import os
+
 from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('imio.project.pst')
 
+PRODUCT_DIR = os.path.dirname(__file__)
+
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
+
+def add_path(path):
+    path = path.strip('/ ')
+    return "%s/%s" % (PRODUCT_DIR, path)
