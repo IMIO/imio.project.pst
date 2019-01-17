c2f9ed9d9b2a9464bcf40eea47ab32f51056c428
Franck Ngaha <franck.o.ngaha@gmail.com>
2019-01-17 12:08:13 +0000

    integrated imio.actionspanel for projectSpaces and projects

--- src/imio/project/pst/browser/actionspanel.py
+++ src/imio/project/pst/browser/actionspanel.py
@@ -1,15 +1,68 @@
 # -*- coding: utf-8 -*-
+from imio.actionspanel.browser.viewlets import ActionsPanelViewlet
 from imio.actionspanel.browser.views import ActionsPanelView
+from zope.component import getMultiAdapter
+
+
+class ProjectSpaceActionsPanelView(ActionsPanelView):
+
+    transitions = [
+        'hide',
+        'reject',
+        'show_internally',
+        'publish_internally',
+    ]
+
+    """
+      This manage the view displaying actions on context.
+    """
+    def __init__(self, context, request):
+        super(ProjectSpaceActionsPanelView, self).__init__(context, request)
+        self.context = context
+        self.request = request
+        self.ACCEPTABLE_ACTIONS = ('paste', 'delete', 'archive', )
+
+    def sortTransitions(self, lst):
+        """ Sort transitions following transitions list order """
+        tr_dict = {}
+        new_lst = []
+        for item in lst:
+            tr_dict[item['id']] = item
+        for transition in self.transitions:
+            if transition in tr_dict:
+                new_lst.append(tr_dict[transition])
+        del lst[:]
+        for item in new_lst:
+            lst.append(item)
 
 
 class SortTransitionsActionsPanelView(ActionsPanelView):
 
     transitions = []
 
+    """
+      This manage the view displaying actions on context.
+    """
+    def __init__(self, context, request):
+        super(SortTransitionsActionsPanelView, self).__init__(context, request)
+        self.context = context
+        self.request = request
+        self.ACCEPTABLE_ACTIONS = ('cut', 'copy', 'paste', 'delete', )
+
     def sortTransitions(self, lst):
         """ Sort transitions following transitions list order """
         tr_order = dict([(val, i) for (i, val) in enumerate(self.transitions)])
         lst.sort(lambda x, y: cmp(tr_order[x['id']], tr_order[y['id']]))
+
+
+class ObjectivesActionsPanelView(SortTransitionsActionsPanelView):
+
+    transitions = [
+        'back_to_created',
+        'back_to_ongoing',
+        'begin',
+        'achieve',
+    ]
 
 
 class PSTActionsPanelView(SortTransitionsActionsPanelView):
@@ -25,11 +78,11 @@ class PSTActionsPanelView(SortTransitionsActionsPanelView):
     ]
 
 
-class ObjectivesActionsPanelView(SortTransitionsActionsPanelView):
+class PstActionsPanelViewlet(ActionsPanelViewlet):
+    """
+        Override render method for pst document
+    """
 
-    transitions = [
-        'back_to_created',
-        'back_to_ongoing',
-        'begin',
-        'achieve',
-    ]
+    def renderViewlet(self):
+        view = getMultiAdapter((self.context, self.request), name='actions_panel')
+        return view(useIcons=False, showTransitions=True, showOwnDelete=False, showAddContent=True)
