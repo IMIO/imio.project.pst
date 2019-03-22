9519a9c1b249691402ee3dba4bd38f2366a8d71d
Stephan Geulette <s.geulette@imio.be>
2019-03-22 15:05:29 +0000

    Increase coverage

--- src/imio/project/pst/tests/test_documentgenerator.py
+++ src/imio/project/pst/tests/test_documentgenerator.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """ documentgenerator.py tests for this package."""
-from datetime import datetime
 
+from imio.project.pst.browser.documentgenerator import DocumentGenerationSOHelper
+from imio.project.pst.testing import IntegrationTestCase
+from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
-from imio.project.pst.testing import IntegrationTestCase
-from imio.project.pst.browser.documentgenerator import DocumentGenerationSOHelper
 
 
 class TestDocumentGenerator(IntegrationTestCase):
@@ -50,6 +50,39 @@ class TestDocumentGenerator(IntegrationTestCase):
         self.assertEqual(len(objs), 2)
         self.assertEqual(objs[0], self.tk1)
 
+        def short_id(helper_i):
+            if helper_i is None:
+                return 'None'
+            return helper_i.real_context.id.split('-')[-1]
+
+        # flattened structure
+        short_flattened_struct = [
+            ('efficient', 'venir', 'population'), ('efficient', 'venir', 'mois'), ('efficient', 'venir', 'soi'),
+            ('efficient', 'communale', 'guidance'), ('efficient', 'communale', 'pmr'),
+            ('efficient', 'communale', 'vous'),
+            ('durable', 'budget', 'energie'), ('durable', 'budget', 'wallonie'), ('durable', 'budget', 'energetiques'),
+            ('durable', '2013', 'communale'), ('durable', '2013', 'batiment'), ('durable', '2013', 'chaleur'),
+            ('securite', '2015', 'sortie')
+        ]
+        # not dashboard
+        del view.request.form['facetedQuery']
+        struct = [(short_id(os), short_id(oo), short_id(ac)) for (os, oo, ac) in view.flatten_structure()]
+        self.assertEqual(len(struct), len(self.pc(portal_type='pstaction')))
+        self.assertListEqual(struct, short_flattened_struct)
+        # empty values
+        new_os = api.content.create(self.pst, 'strategicobjective', 'new_os', 'New OS')
+        struct = [(short_id(os), short_id(oo), short_id(ac)) for (os, oo, ac) in view.flatten_structure()]
+        self.assertListEqual(struct, short_flattened_struct + [('new_os', 'None', 'None')])
+        api.content.create(new_os, 'operationalobjective', 'new_oo', 'New OO')
+        struct = [(short_id(os), short_id(oo), short_id(ac)) for (os, oo, ac) in view.flatten_structure()]
+        self.assertListEqual(struct, short_flattened_struct + [('new_os', 'new_oo', 'None')])
+        # dashboard
+        view.request.form['facetedQuery'] = ''
+        brains = self.pc(id='etre-une-commune-qui-offre-un-service-public-moderne-efficace-et-efficient')
+        view.context_var = lambda x, default='', brains=brains: brains
+        struct = [(short_id(os), short_id(oo), short_id(ac)) for (os, oo, ac) in view.flatten_structure()]
+        self.assertListEqual(struct, short_flattened_struct[:6])
+
     def test_DocumentGenerationSOHelper(self):
         view = self.os1.unrestrictedTraverse('@@document_generation_helper_view')
         # not on dashboard
@@ -69,6 +102,7 @@ class TestDocumentGenerator(IntegrationTestCase):
         self.assertEqual(view.getOwnBudget(), '<table><thead><tr><th>Budget_type</th><th>Year</th><th>Amount</th></tr>'
                          '</thead><tbody><tr><td>Wallonie</td><td>2019</td><td>1,000.0</td><td></td></tr>'
                          '</tbody></table>')
+        self.assertEqual(view.getOwnBudgetAsText(), '2019 pour Wallonie: 1000€')
         self.assertTrue(view.hasChildrenBudget(self.os1))
         self.assertIn('<tfoot><tr><td>Totals</td><td>32767.5</td><td>27860.0</td><td>4907.5</td><td>-</td><td>-</td>'
                       '<td>-</td><td>-</td></tr></tfoot>', view.getChildrenBudget())
@@ -147,6 +181,24 @@ class TestDocumentGenerator(IntegrationTestCase):
         objs = view.getTasks(self.ac1)
         self.assertEqual(len(objs), 2)
         self.assertEqual(objs[0], self.tk1)
+        # on dashboard
+        view.request.form['facetedQuery'] = ''
+        self.assertTrue(view.is_dashboard())
+        brains = self.pc(path={'query': '/'.join(self.ac1.getPhysicalPath()), 'depth': 1})
+        view.uids_to_objs(brains)
+        self.assertListEqual(view.objs, [self.ac1['rediger-le-profil-de-fonction'], self.tk1])
+        self.assertEqual(view.sel_type, 'task')
+        objs = view.getStrategicObjectives()
+        self.assertEqual(len(objs), 1)
+        self.assertEqual(objs[0], self.os1)
+        objs = view.getOperationalObjectives(so=self.os1)
+        self.assertEqual(len(objs), 1)
+        self.assertEqual(objs[0], self.oo1)
+        objs = view.getActions(self.oo1)
+        self.assertEqual(len(objs), 1)
+        self.assertEqual(objs[0], self.ac1)
+        objs = view.getTasks(self.ac1)
+        self.assertEqual(len(objs), 2)
 
     def test_CategoriesDocumentGenerationView(self):
         pod_template = self.portal['templates']['ddetail']
