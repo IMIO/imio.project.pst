f02f5582b55ccd419394245ce606dc40095525d6
Stephan Geulette <s.geulette@imio.be>
2019-06-23 20:51:05 +0000

    Preparing release 1.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='imio.project.pst',
-    version='1.2.dev0',
+    version='1.2',
     description="PST management",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
