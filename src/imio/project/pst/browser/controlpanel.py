0a7ff9eeb315007411d74bda7af0d53d48ed052b
Stephan Geulette <s.geulette@imio.be>
2020-02-05 09:57:37 +0000

    Added mandatory fields check

--- src/imio/project/pst/browser/controlpanel.py
+++ src/imio/project/pst/browser/controlpanel.py
@@ -2,6 +2,7 @@
 
 from imio.project.core import _ as _c
 from imio.project.core.browser.controlpanel import get_pt_fields_voc
+from imio.project.core.browser.controlpanel import mandatory_check
 from imio.project.pst import _
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
@@ -15,6 +16,13 @@ from zope.interface import Invalid
 from zope.schema.interfaces import IVocabularyFactory
 
 
+mandatory_fields = {'strategicobjective': ['IDublinCore.title', 'IDublinCore.description'],
+#                    'operationalobjective': ['IDublinCore.title', 'IDublinCore.description'],
+#                    'pstaction': [],
+#                    'pstsubaction': [],
+                    }
+
+
 class SOFieldsVocabulary(object):
     implements(IVocabularyFactory)
 
@@ -25,7 +33,8 @@ class SOFieldsVocabulary(object):
                                   'IDublinCore.subjects', 'INameFromTitle.title', 'IVersionable.changeNote',
                                   'effective_begin_date', 'effective_end_date', 'extra_concerned_people', 'manager',
                                   'notes', 'planned_begin_date', 'planned_end_date', 'priority', 'progress',
-                                  'result_indicator', 'visible_for'])
+                                  'result_indicator', 'visible_for'],
+                                 mandatory_fields)
 
 
 class IImioPSTSettings(Interface):
@@ -33,18 +42,14 @@ class IImioPSTSettings(Interface):
 
     strategicobjective_fields = schema.List(
         title=_c(u"${type} fields display", mapping={'type': _('StrategicObjective')}),
+        description=_c(u'Put fields on the right to display it. Fields with asterisk are mandatory !'),
         value_type=schema.Choice(vocabulary=u'imio.project.pst.SOFieldsVocabulary'),
 #        value_type=schema.Choice(source=IMFields),  # a source is not managed by registry !!
     )
 
     @invariant
     def validateSettings(data):
-        # voir imio.actionspanel.browser.registry
-        if not data._Data_data___:
-            return
-
-        if not data.strategicobjective_fields:
-            raise Invalid(_(u'Error!'))
+        mandatory_check(data, mandatory_fields)
 
 
 class SettingsEditForm(RegistryEditForm):
