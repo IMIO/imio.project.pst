ddebb5e1a582038a0d8ec99385307c84d49a6861
fngaha <franck.o.ngaha@gmail.com>
2020-04-22 22:03:24 +0200

    Created a pst project space
    PRJ-402
    Moved "* _fields" fields, validators and vocabularies
    from the control panel to the pst project space

--- src/imio/project/pst/browser/controlpanel.py
+++ src/imio/project/pst/browser/controlpanel.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from imio.project.core import _ as _c
-from imio.project.core.browser.controlpanel import get_pt_fields_voc
 from imio.project.core.browser.controlpanel import mandatory_check
 from imio.project.core.browser.controlpanel import position_check
 from imio.project.pst import _
@@ -35,104 +34,25 @@ field_constraints = {
 }
 
 
-class SOFieldsVocabulary(object):
-    implements(IVocabularyFactory)
-
-    def __call__(self, context):
-        return get_pt_fields_voc('strategicobjective',
-                                 ['IDublinCore.description', 'IDublinCore.contributors', 'IDublinCore.creators',
-                                  'IDublinCore.effective', 'IDublinCore.expires', 'IDublinCore.language',
-                                  'IDublinCore.rights', 'IDublinCore.subjects', 'INameFromTitle.title',
-                                  'IVersionable.changeNote', 'effective_begin_date', 'effective_end_date',
-                                  'extra_concerned_people', 'manager', 'notes', 'planned_begin_date',
-                                  'planned_end_date', 'priority', 'progress', 'result_indicator', 'visible_for'],
-                                 field_constraints)
-
-
-class OOFieldsVocabulary(object):
-    implements(IVocabularyFactory)
-
-    def __call__(self, context):
-        return get_pt_fields_voc('operationalobjective',
-                                 ['IDublinCore.description', 'IDublinCore.contributors', 'IDublinCore.creators',
-                                  'IDublinCore.effective', 'IDublinCore.expires', 'IDublinCore.language',
-                                  'IDublinCore.rights', 'IDublinCore.subjects', 'INameFromTitle.title',
-                                  'IVersionable.changeNote', 'effective_begin_date', 'effective_end_date', 'notes',
-                                  'planned_begin_date', 'progress', 'visible_for'],
-                                 field_constraints)
-
-
-class ActionFieldsVocabulary(object):
-    implements(IVocabularyFactory)
-
-    def __call__(self, context):
-        return get_pt_fields_voc('pstaction',
-                                 ['IDublinCore.description', 'IDublinCore.contributors', 'IDublinCore.creators',
-                                  'IDublinCore.effective', 'IDublinCore.expires', 'IDublinCore.language',
-                                  'IDublinCore.rights', 'IDublinCore.subjects', 'INameFromTitle.title',
-                                  'IVersionable.changeNote', 'notes', 'priority', 'visible_for'],
-                                 field_constraints)
-
-
-class SubActionFieldsVocabulary(object):
-    """ Not used """
-    implements(IVocabularyFactory)
-
-    def __call__(self, context):
-        return get_pt_fields_voc('pstsubaction',
-                                 ['IDublinCore.description', 'IDublinCore.contributors', 'IDublinCore.creators',
-                                  'IDublinCore.effective', 'IDublinCore.expires', 'IDublinCore.language',
-                                  'IDublinCore.rights', 'IDublinCore.subjects', 'INameFromTitle.title',
-                                  'IVersionable.changeNote', 'notes', 'priority', 'visible_for'],
-                                 field_constraints)
-
-
 class IImioPSTSettings(Interface):
     """"""
-
-    strategicobjective_fields = schema.List(
-        title=_c(u"${type} fields display", mapping={'type': _('StrategicObjective')}),
-        description=_c(u"Put fields on the right to display it. Flags are : ..."),
-        value_type=schema.Choice(vocabulary=u'imio.project.pst.SOFieldsVocabulary'),
-#        value_type=schema.Choice(source=IMFields),  # a source is not managed by registry !!
-    )
-
-    operationalobjective_fields = schema.List(
-        title=_c(u"${type} fields display", mapping={'type': _('OperationalObjective')}),
-#        description=_c(u'Put fields on the right to display it. Flags are : ...'),
-        value_type=schema.Choice(vocabulary=u'imio.project.pst.OOFieldsVocabulary'),
-    )
-
-    pstaction_fields = schema.List(
-        title=_c(u"${type} fields display", mapping={'type': _('PSTAction')}),
-#        description=_c(u'Put fields on the right to display it. Flags are : ...'),
-        value_type=schema.Choice(vocabulary=u'imio.project.pst.ActionFieldsVocabulary'),
-    )
-
-    # this field will be hidden
-    pstsubaction_fields = schema.List(
-        title=_c(u"${type} fields display", mapping={'type': _('PSTSubAction')}),
-#        description=_c(u'Put fields on the right to display it. Flags are : ...'),
-        value_type=schema.Choice(vocabulary=u'imio.project.pst.ActionFieldsVocabulary'),
-    )
-
     strategicobjective_budget_states = schema.List(
         title=_c(u"${type} budget globalization states", mapping={'type': _('StrategicObjective')}),
-        description=_(u'Put states on the right for which you want to globalize budget fields.'),
+        description=_c(u'Put states on the right for which you want to globalize budget fields.'),
         required=False,
         value_type=schema.Choice(vocabulary=u'imio.project.pst.SOReviewStatesVocabulary'),
     )
 
     operationalobjective_budget_states = schema.List(
         title=_c(u"${type} budget globalization states", mapping={'type': _('OperationalObjective')}),
-#        description=_(u'Put states on the right for which you want to globalize budget fields.'),
+#        description=_c(u'Put states on the right for which you want to globalize budget fields.'),
         required=False,
         value_type=schema.Choice(vocabulary=u'imio.project.pst.OOReviewStatesVocabulary'),
     )
 
     pstaction_budget_states = schema.List(
         title=_c(u"${type} budget globalization states", mapping={'type': _('PSTAction')}),
-#        description=_(u'Put states on the right for which you want to globalize budget fields.'),
+#        description=_c(u'Put states on the right for which you want to globalize budget fields.'),
         required=False,
         value_type=schema.Choice(vocabulary=u'imio.project.pst.PSTActionReviewStatesVocabulary'),
     )
@@ -140,7 +60,7 @@ class IImioPSTSettings(Interface):
     # this field will be hidden
     pstsubaction_budget_states = schema.List(
         title=_c(u"${type} budget globalization states", mapping={'type': _('PSTSubAction')}),
-#        description=_(u'Put states on the right for which you want to globalize budget fields.'),
+#        description=_c(u'Put states on the right for which you want to globalize budget fields.'),
         required=False,
         value_type=schema.Choice(vocabulary=u'imio.project.pst.PSTActionReviewStatesVocabulary'),
     )
---
+++ src/imio/project/pst/content/pstprojectspace.py
@@ -0,0 +1,112 @@
+from imio.project.core.browser.controlpanel import get_pt_fields_voc
+from imio.project.core.browser.controlpanel import mandatory_check
+from imio.project.core.browser.controlpanel import position_check
+from imio.project.core.content.projectspace import IProjectSpace
+from imio.project.core.content.projectspace import ProjectSpace
+from imio.project.core.content.projectspace import ProjectSpaceSchemaPolicy
+from imio.project.core import _ as _c
+from imio.project.pst import _
+from zope import schema
+from zope.interface import implements
+from zope.interface import invariant
+from zope.schema.interfaces import IVocabularyFactory
+
+
+class IPSTProjectSpace(IProjectSpace):
+    """
+        PST Project schema
+    """
+    strategicobjective_fields = schema.List(
+        title=_c(u"${type} fields display", mapping={'type': _('StrategicObjective')}),
+        description=_c(u"Put fields on the right to display it. Flags are : ..."),
+        value_type=schema.Choice(vocabulary=u'imio.project.pst.SOFieldsVocabulary'),
+#        value_type=schema.Choice(source=IMFields),  # a source is not managed by registry !!
+    )
+
+    operationalobjective_fields = schema.List(
+        title=_c(u"${type} fields display", mapping={'type': _('OperationalObjective')}),
+#        description=_c(u'Put fields on the right to display it. Flags are : ...'),
+        value_type=schema.Choice(vocabulary=u'imio.project.pst.OOFieldsVocabulary'),
+    )
+
+    pstaction_fields = schema.List(
+        title=_c(u"${type} fields display", mapping={'type': _('PSTAction')}),
+#        description=_c(u'Put fields on the right to display it. Flags are : ...'),
+        value_type=schema.Choice(vocabulary=u'imio.project.pst.ActionFieldsVocabulary'),
+    )
+
+    # this field will be hidden
+    pstsubaction_fields = schema.List(
+        title=_c(u"${type} fields display", mapping={'type': _('PSTSubAction')}),
+#        description=_c(u'Put fields on the right to display it. Flags are : ...'),
+        value_type=schema.Choice(vocabulary=u'imio.project.pst.ActionFieldsVocabulary'),
+    )
+
+    @invariant
+    def validateSettings(data):
+        mandatory_check(data, field_constraints)
+        position_check(data, field_constraints)
+
+
+class PSTProjectSpace(ProjectSpace):
+    """ """
+    implements(IPSTProjectSpace)
+
+
+class PSTProjectSpaceSchemaPolicy(ProjectSpaceSchemaPolicy):
+    """ """
+
+    def bases(self, schemaName, tree):
+        return (IPSTProjectSpace, )
+
+
+class SOFieldsVocabulary(object):
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        return get_pt_fields_voc('strategicobjective',
+                                 ['IDublinCore.description', 'IDublinCore.contributors', 'IDublinCore.creators',
+                                  'IDublinCore.effective', 'IDublinCore.expires', 'IDublinCore.language',
+                                  'IDublinCore.rights', 'IDublinCore.subjects', 'INameFromTitle.title',
+                                  'IVersionable.changeNote', 'effective_begin_date', 'effective_end_date',
+                                  'extra_concerned_people', 'manager', 'notes', 'planned_begin_date',
+                                  'planned_end_date', 'priority', 'progress', 'result_indicator', 'visible_for'],
+                                 field_constraints)
+
+
+class OOFieldsVocabulary(object):
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        return get_pt_fields_voc('operationalobjective',
+                                 ['IDublinCore.description', 'IDublinCore.contributors', 'IDublinCore.creators',
+                                  'IDublinCore.effective', 'IDublinCore.expires', 'IDublinCore.language',
+                                  'IDublinCore.rights', 'IDublinCore.subjects', 'INameFromTitle.title',
+                                  'IVersionable.changeNote', 'effective_begin_date', 'effective_end_date', 'notes',
+                                  'planned_begin_date', 'progress', 'visible_for'],
+                                 field_constraints)
+
+
+class ActionFieldsVocabulary(object):
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        return get_pt_fields_voc('pstaction',
+                                 ['IDublinCore.description', 'IDublinCore.contributors', 'IDublinCore.creators',
+                                  'IDublinCore.effective', 'IDublinCore.expires', 'IDublinCore.language',
+                                  'IDublinCore.rights', 'IDublinCore.subjects', 'INameFromTitle.title',
+                                  'IVersionable.changeNote', 'notes', 'priority', 'visible_for'],
+                                 field_constraints)
+
+
+class SubActionFieldsVocabulary(object):
+    """ Not used """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        return get_pt_fields_voc('pstsubaction',
+                                 ['IDublinCore.description', 'IDublinCore.contributors', 'IDublinCore.creators',
+                                  'IDublinCore.effective', 'IDublinCore.expires', 'IDublinCore.language',
+                                  'IDublinCore.rights', 'IDublinCore.subjects', 'INameFromTitle.title',
+                                  'IVersionable.changeNote', 'notes', 'priority', 'visible_for'],
+                                 field_constraints)
