c839a7c1a672e3935c26073662f874436be329c5
fngaha <franck.o.ngaha@gmail.com>
2020-06-14 21:33:42 +0200

    Added fields for dashboard collection columns config
    PRJ-402

--- src/imio/project/pst/content/pstprojectspace.py
+++ src/imio/project/pst/content/pstprojectspace.py
@@ -1,4 +1,5 @@
 from plone.autoform import directives
+from collective.eeafaceted.z3ctable import _ as _z
 from imio.project.core.content.projectspace import get_pt_fields_voc
 from imio.project.core.content.projectspace import mandatory_check
 from imio.project.core.content.projectspace import position_check
@@ -11,6 +12,8 @@ from zope import schema
 from zope.interface import implements
 from zope.interface import invariant
 from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
 
 
 field_constraints = {
@@ -28,6 +31,64 @@ field_constraints = {
               'pstaction': [],
               },
 }
+
+
+StrategicObjectivesColumnsVocabulary = SimpleVocabulary(
+    [SimpleTerm(value=u'select_row', title=_z(u'select_row')),
+     SimpleTerm(value=u'pretty_link', title=_z(u'pretty_link')),
+     SimpleTerm(value=u'review_state', title=_z(u'header_review_state')),
+     SimpleTerm(value=u'categories', title=_c(u'Categories')),
+     SimpleTerm(value=u'ModificationDate', title=_z(u'header_ModificationDate')),
+     SimpleTerm(value=u'history_actions', title=_z(u'history_actions'))]
+)
+
+
+OperationalObjectivesColumnsVocabulary = SimpleVocabulary(
+    [SimpleTerm(value=u'select_row', title=_z(u'select_row')),
+     SimpleTerm(value=u'pretty_link', title=_z(u'pretty_link')),
+     SimpleTerm(value=u'parents', title=_z(u'header_parents')),
+     SimpleTerm(value=u'review_state', title=_z(u'header_review_state')),
+     SimpleTerm(value=u'manager', title=_z(u'header_manager')),
+     SimpleTerm(value=u'planned_end_date', title=_z(u'header_planned_end_date')),
+     SimpleTerm(value=u'priority', title=_z(u'header_priority')),
+     SimpleTerm(value=u'categories', title=_c(u'Categories')),
+     SimpleTerm(value=u'sdgs', title=_z(u'header_sdgs')),
+     SimpleTerm(value=u'ModificationDate', title=_z(u'header_ModificationDate')),
+     SimpleTerm(value=u'history_actions', title=_z(u'history_actions'))]
+)
+
+
+PstactionColumnsVocabulary = SimpleVocabulary(
+    [SimpleTerm(value=u'select_row', title=_z(u'select_row')),
+     SimpleTerm(value=u'pretty_link', title=_z(u'pretty_link')),
+     SimpleTerm(value=u'parents', title=_z(u'header_parents')),
+     SimpleTerm(value=u'review_state', title=_z(u'header_review_state')),
+     SimpleTerm(value=u'manager', title=_z(u'header_manager')),
+     SimpleTerm(value=u'responsible', title=_z(u'header_responsible')),
+     SimpleTerm(value=u'planned_begin_date', title=_z(u'header_planned_begin_date')),
+     SimpleTerm(value=u'planned_end_date', title=_z(u'header_planned_end_date')),
+     SimpleTerm(value=u'effective_begin_date', title=_z(u'header_effective_begin_date')),
+     SimpleTerm(value=u'effective_end_date', title=_z(u'header_effective_end_date')),
+     SimpleTerm(value=u'progress', title=_z(u'header_progress')),
+     SimpleTerm(value=u'health_indicator', title=_z(u'header_health_indicator')),
+     SimpleTerm(value=u'sdgs', title=_z(u'header_sdgs')),
+     SimpleTerm(value=u'ModificationDate', title=_z(u'header_ModificationDate')),
+     SimpleTerm(value=u'history_actions', title=_z(u'history_actions'))]
+)
+
+
+TasksColumnsVocabulary = SimpleVocabulary(
+    [SimpleTerm(value=u'select_row', title=_z(u'select_row')),
+     SimpleTerm(value=u'pretty_link', title=_z(u'pretty_link')),
+     SimpleTerm(value=u'parents', title=_z(u'header_parents')),
+     SimpleTerm(value=u'review_state', title=_z(u'header_review_state')),
+     SimpleTerm(value=u'assigned_group', title=_z(u'header_assigned_group')),
+     SimpleTerm(value=u'assigned_user', title=_z(u'header_assigned_user')),
+     SimpleTerm(value=u'due_date', title=_z(u'header_due_date')),
+     SimpleTerm(value=u'CreationDate', title=_z(u'header_CreationDate')),
+     SimpleTerm(value=u'ModificationDate', title=_z(u'header_ModificationDate')),
+     SimpleTerm(value=u'history_actions', title=_z(u'history_actions'))]
+)
 
 
 class IPSTProjectSpace(IProjectSpace):
@@ -58,6 +119,36 @@ class IPSTProjectSpace(IProjectSpace):
         title=_c(u"${type} fields display", mapping={'type': _('PSTSubAction')}),
 #        description=_c(u'Put fields on the right to display it. Flags are : ...'),
         value_type=schema.Choice(vocabulary=u'imio.project.pst.ActionFieldsVocabulary'),
+    )
+
+    strategicobjective_columns = schema.List(
+        title=_(u"StrategicObjective columns"),
+#        description=_c(u'Put fields on the right to display it. Flags are : ...'),
+        value_type=schema.Choice(vocabulary=StrategicObjectivesColumnsVocabulary),
+    )
+
+    operationalobjective_columns = schema.List(
+        title=_(u"OperationalObjective columns"),
+#        description=_c(u'Put fields on the right to display it. Flags are : ...'),
+        value_type=schema.Choice(vocabulary=OperationalObjectivesColumnsVocabulary),
+    )
+
+    pstaction_columns = schema.List(
+        title=_(u"PSTAction columns"),
+#        description=_c(u'Put fields on the right to display it. Flags are : ...'),
+        value_type=schema.Choice(vocabulary=PstactionColumnsVocabulary),
+    )
+
+    pstsubaction_columns = schema.List(
+        title=_(u"PSTSubAction columns"),
+#        description=_c(u'Put fields on the right to display it. Flags are : ...'),
+        value_type=schema.Choice(vocabulary=PstactionColumnsVocabulary),
+    )
+
+    tasks_columns = schema.List(
+        title=_(u"Tasks columns"),
+#        description=_c(u'Put fields on the right to display it. Flags are : ...'),
+        value_type=schema.Choice(vocabulary=TasksColumnsVocabulary),
     )
 
     directives.omitted('project_fields')
