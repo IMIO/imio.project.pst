612d667352661edd27e3c614bca39712dda840ef
fngaha <franck.o.ngaha@gmail.com>
2020-10-25 16:57:26 +0100

    Tested type of fields
    PRJLIEA-4

--- src/imio/project/pst/tests/test_action.py
+++ src/imio/project/pst/tests/test_action.py
@@ -2,10 +2,13 @@
 """ Content action tests for this package."""
 
 from imio.project.pst.content import action
+from imio.project.pst.content.pstprojectspace import PSTACTION_EXCLUDED_FIELDS
 from imio.project.pst.testing import IntegrationTestCase
 from plone import api
 from plone.app.testing import TEST_USER_NAME
+from zope.app.content import queryContentType
 from zope.interface import Invalid
+from zope.schema import getFieldsInOrder
 from zope.schema._bootstrapinterfaces import RequiredMissing
 from zope.schema._bootstrapinterfaces import TooShort
 
@@ -68,3 +71,18 @@ class TestAction(IntegrationTestCase):
         self.assertEquals(raised.exception.message,
                           u'You must choose at least one group of which you are a member')
         validator.validate(self.groups.values())
+
+    def test_pstaction_fields_type(self):
+        """Test of fields type."""
+        schema = queryContentType(self.ac1)
+        fields = getFieldsInOrder(schema)
+        # field is a tuple : (field_name, field_obj)
+        for field in fields:
+            try:
+                # pstaction_fields_type is a dict : {'field_name': field_class}
+                self.assertTrue(isinstance(field[1], self.pstaction_fields_class[field[0]]))
+            except KeyError as err:
+                # its ok for fields excluded intentionally
+                print('EXCLUDED : {}'.format(err.message))
+                if err.message in PSTACTION_EXCLUDED_FIELDS:
+                    pass
---
+++ src/imio/project/pst/tests/test_strategic.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+""" Content action tests for this package."""
+
+from imio.project.pst.content.pstprojectspace import STRATEGICOBJECTIVE_EXCLUDED_FIELDS
+from imio.project.pst.testing import IntegrationTestCase
+from zope.app.content import queryContentType
+from zope.schema import getFieldsInOrder
+
+
+class TestStrategic(IntegrationTestCase):
+    """Test strategic.py."""
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        super(TestStrategic, self).setUp()
+
+    def test_strategicobjective_fields_type(self):
+        """Test of fields type."""
+        schema = queryContentType(self.os1)
+        fields = getFieldsInOrder(schema)
+        # field is a tuple : (field_name, field_obj)
+        for field in fields:
+            try:
+                # a_fields_type is a dict : {'field_name': field_class}
+                self.assertTrue(isinstance(field[1], self.strategicobjective_fields_class[field[0]]))
+            except KeyError as err:
+                # its ok for fields excluded intentionally
+                print('EXCLUDED : {}'.format(err.message))
+                if err.message in STRATEGICOBJECTIVE_EXCLUDED_FIELDS:
+                    pass
