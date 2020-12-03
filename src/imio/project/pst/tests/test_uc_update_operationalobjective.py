b309d23a83a8e01700225150f756b710d611090a
fngaha <franck.o.ngaha@gmail.com>
2020-12-03 13:55:33 +0100

    Updated test, adding scenario PRJ-442

--- src/imio/project/pst/tests/test_uc_update_operationalobjective.py
+++ src/imio/project/pst/tests/test_uc_update_operationalobjective.py
@@ -4,7 +4,7 @@ from ftw.testbrowser.pages import statusmessages
 from imio.project.pst.testing import FunctionalTestCase
 
 
-def step_a1_3(browser):
+def step_3_a1(browser):
     """The user cancel the form."""
     form = browser.forms['form']
     form.find_button_by_label('Annuler').click()
@@ -18,7 +18,7 @@ class TestUpdateOperationalObjective(FunctionalTestCase):
                  an administrative manager as well as an administrator
     Author: Franck Ngaha
     Created: 25/11/2020
-    Updated: 26/11/2020
+    Updated: 03/12/2020
     Preconditions: The user must be authenticated as a global editor or administrative manager or administrator
     Start: The user is on the page of a operational objective
     """
@@ -48,8 +48,8 @@ class TestUpdateOperationalObjective(FunctionalTestCase):
         self.start_up(browser)
         self.step_1(browser)
         self.step_2(browser)
-        step_a1_3(browser)  # The user cancel the form
-        self.step_a1_4(browser)  # The system back to the previous page with "Addition canceled" Info
+        step_3_a1(browser)  # The user cancel the form
+        self.step_4_a1(browser)  # The system back to the previous page with "Addition canceled" Info
 
     @browsing
     def test_alternative_a2(self, browser):
@@ -58,8 +58,18 @@ class TestUpdateOperationalObjective(FunctionalTestCase):
         self.start_up(browser)
         self.step_1(browser)
         self.step_2(browser)
-        self.step_a2_3(browser)  # The user removes the deadline
-        self.step_a2_4(browser)  # The system updates element with warning message
+        self.step_3_a2(browser)  # The user removes the deadline
+        self.step_4_a2(browser)  # The system displays the max planned end date of the contained items and warn
+
+    @browsing
+    def test_alternative_a3(self, browser):
+        """The user fills a planned end date smaller than the largest of their contained actions."""
+        self.preconditions(browser)
+        self.start_up(browser)
+        self.step_1(browser)
+        self.step_2(browser)
+        self.step_3_a3(browser)  # The user fills a smaller planned end date
+        self.step_4_a3(browser)  # The system updates element with warning message
 
     def preconditions(self, browser):
         """Login as psteditor."""
@@ -88,7 +98,7 @@ class TestUpdateOperationalObjective(FunctionalTestCase):
         fields[self.title_form_widget_name] = u'Titre'
         form.find_button_by_label('Sauvegarder').click()
 
-    def step_a2_3(self, browser):
+    def step_3_a2(self, browser):
         """The user remove the deadline and save."""
         form = browser.forms['form']
         fields = form.values
@@ -97,13 +107,22 @@ class TestUpdateOperationalObjective(FunctionalTestCase):
         fields[self.planned_end_date_year_form_widget_name] = u''
         form.find_button_by_label('Sauvegarder').click()
 
+    def step_3_a3(self, browser):
+        """The user fills a smaller planned end date."""
+        form = browser.forms['form']
+        fields = form.values
+        fields[self.planned_end_date_day_form_widget_name] = u'30'
+        fields[self.planned_end_date_month_form_widget_name] = u'6'
+        fields[self.planned_end_date_year_form_widget_name] = u'2023'
+        form.find_button_by_label('Sauvegarder').click()
+
     def step_4(self, browser):
         """The system save changes with "Modify changes" info success."""
         heading = browser.css('.documentFirstHeading').first
         self.assertEqual('Titre (OO.15)', heading.text)
         statusmessages.assert_message(u'Modifications sauvegardées')
 
-    def step_a1_4(self, browser):
+    def step_4_a1(self, browser):
         """The system back to the previous page with 'Modification canceled' Info."""
         heading = browser.css('.documentFirstHeading').first
         self.assertEqual(
@@ -111,7 +130,7 @@ class TestUpdateOperationalObjective(FunctionalTestCase):
             heading.text)
         statusmessages.assert_message(u'Modification annulée')
 
-    def step_a2_4(self, browser):
+    def step_4_a2(self, browser):
         """The system displays the max planned end date of the contained items with a warning message."""
         heading = browser.css('.documentFirstHeading').first
         self.assertEqual(
@@ -125,3 +144,17 @@ class TestUpdateOperationalObjective(FunctionalTestCase):
         )
         statusmessages.assert_message(u'Modifications sauvegardées')
         self.assertEqual(browser.css('#form-widgets-planned_end_date').text, ['30/06/2024'])
+        
+    def step_4_a3(self, browser):
+        """The system updates element with warning message."""
+        heading = browser.css('.documentFirstHeading').first
+        self.assertEqual(
+            u"Réduire la consommation énergétique des bâtiments communaux de 20% d'ici 2024 (OO.15)",
+            heading.text
+        )
+        self.assertEqual(
+            browser.css('#messagesviewlet').text,
+            [u"Avertissement L'échéance d'au moins une action est supérieure à l'échéance de l'objectif opérationnel"]
+        )
+        statusmessages.assert_message(u'Modifications sauvegardées')
+        self.assertEqual(browser.css('#form-widgets-planned_end_date').text, ['30/06/2023'])
