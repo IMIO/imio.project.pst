f99f0de5fe05d3e03e382b65a2cade18aac7ad85
fngaha <franck.o.ngaha@gmail.com>
2021-01-23 18:44:48 +0100

    Refactored, updated and add utils module tests PRJ-442

--- src/imio/project/pst/tests/test_operational.py
+++ src/imio/project/pst/tests/test_operational.py
@@ -33,37 +33,3 @@ class TestOperational(IntegrationTestCase):
                 print('EXCLUDED : {}'.format(err.message))
                 if err.message in OPERATIONALOBJECTIVE_EXCLUDED_FIELDS:
                     pass
-
-    def test_list_contained_brains(self):
-        """Test list_contained_brains method on operationalobjective."""
-        self.assertEqual(
-            [brain.Title for brain in
-             self.oo_15.list_contained_brains(["pstaction", "action_link", "pstsubaction", "subaction_link"])],
-            ["R\xc3\xa9duire la consommation \xc3\xa9nerg\xc3\xa9tique de l'administration communale (A.16)",
-             'R\xc3\xa9aliser un audit \xc3\xa9nerg\xc3\xa9tique du b\xc3\xa2timent (SA.17)',
-             "En fonction des r\xc3\xa9sultats, proc\xc3\xa9der \xc3\xa0 l'isolation du b\xc3\xa2timent (SA.18)",
-             'En fonction des r\xc3\xa9sultats, remplacer le syst\xc3\xa8me de chauffage (SA.19)',
-             'R\xc3\xa9duire la consommation \xc3\xa9nerg\xc3\xa9tique du hangar communal (A.20)',
-             'R\xc3\xa9aliser un audit \xc3\xa9nerg\xc3\xa9tique du b\xc3\xa2timent (SA.17)',
-             "En fonction des r\xc3\xa9sultats, proc\xc3\xa9der \xc3\xa0 l'isolation du b\xc3\xa2timent (SA.18)",
-             'En fonction des r\xc3\xa9sultats, remplacer le syst\xc3\xa8me de chauffage (SA.19)']
-        )
-
-    def test_list_planned_end_date_of_contained_brains(self):
-        """Test list_planned_end_date_of_contained_brains method on operationalobjective."""
-        self.assertEqual(
-            self.oo_15.list_planned_end_date_of_contained_brains(
-                ["pstaction", "action_link", "pstsubaction", "subaction_link"]),
-            [datetime.date(datetime(2024, 6, 30)), datetime.date(datetime(2020, 6, 30)),
-             datetime.date(datetime(2020, 10, 31)), datetime.date(datetime(2020, 10, 31)),
-             datetime.date(datetime(2024, 6, 30)), datetime.date(datetime(2020, 6, 30)),
-             datetime.date(datetime(2020, 10, 31)), datetime.date(datetime(2020, 10, 31))]
-        )
-
-    def test_get_max_planned_end_date_of_contained_brains(self):
-        """Test get_max_planned_end_date_of_contained_brains method on operationalobjective."""
-        self.assertEqual(
-            self.oo_15.get_max_planned_end_date_of_contained_brains(
-                ["pstaction", "action_link", "pstsubaction", "subaction_link"]),
-            datetime.date(datetime(2024, 6, 30))
-        )
---
+++ src/imio/project/pst/tests/test_utils.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+""" Content action tests for this package."""
+from datetime import datetime
+
+from imio.project.pst.testing import IntegrationTestCase
+from imio.project.pst.utils import find_deadlines_on_children, find_max_deadline_on_children, find_brains_on_parents, \
+    find_deadlines_on_parents, find_max_deadline_on_parents
+
+
+class TestUtils(IntegrationTestCase):
+    """Test action.py."""
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        super(TestUtils, self).setUp()
+        self.os_10 = self.pst['etre-une-commune-qui-sinscrit-dans-la-lignee-des-accords-de-reductions-des-gaz-a-effet-'
+                              'de-serre-afin-dassurer-le-developpement-durable']
+        self.oo_15 = self.os_10['reduire-la-consommation-energetique-des-batiments-communaux-de-20-dici-2024']
+        self.a_16 = self.oo_15['reduire-la-consommation-energetique-de-ladministration-communale']
+        self.sa_17 = self.a_16['realiser-un-audit-energetique-du-batiment']
+        self.task = self.sa_17['ecrire-le-cahier-des-charges']
+
+    def test_find_deadlines_on_operational_objective_children(self):
+        """Test find_deadlines_on_operational_objective_children method on operational objective."""
+        self.assertEqual(
+            find_deadlines_on_children(
+                self.oo_15,
+                {
+                    "pstaction": "planned_end_date",
+                    "pstsubaction": "planned_end_date",
+                    "subaction_link": "planned_end_date",
+                    "task": "due_date"
+                }
+            ),
+            [
+                datetime.date(datetime(2024, 6, 30)),
+                datetime.date(datetime(2020, 6, 30)),
+                datetime.date(datetime(2020, 4, 30)),
+                datetime.date(datetime(2020, 3, 31)),
+                datetime.date(datetime(2020, 10, 31)),
+                datetime.date(datetime(2020, 10, 31)),
+                datetime.date(datetime(2024, 6, 30)),
+                datetime.date(datetime(2020, 4, 30)),
+                datetime.date(datetime(2020, 3, 31)),
+                datetime.date(datetime(2020, 6, 30)),
+                datetime.date(datetime(2020, 10, 31)),
+                datetime.date(datetime(2020, 10, 31)),
+            ]
+        )
+
+    def test_find_deadlines_on_pst_action_children(self):
+        """Test find_deadlines_on_pst_action_children method on pst action."""
+        self.assertEqual(
+            find_deadlines_on_children(
+                self.a_16,
+                {
+                    "pstsubaction": "planned_end_date",
+                    "subaction_link": "planned_end_date",
+                    "task": "due_date"
+                }
+            ),
+            [
+                datetime.date(datetime(2020, 6, 30)),
+                datetime.date(datetime(2020, 4, 30)),
+                datetime.date(datetime(2020, 3, 31)),
+                datetime.date(datetime(2020, 10, 31)),
+                datetime.date(datetime(2020, 10, 31))
+            ]
+        )
+
+    def test_find_deadlines_on_pst_sub_action_children(self):
+        """Test find_deadlines_on_pst_sub_action_children method on pst sub action."""
+        self.assertEqual(
+            find_deadlines_on_children(self.sa_17, {"task": "due_date"}),
+            [datetime.date(datetime(2020, 4, 30)), datetime.date(datetime(2020, 3, 31))]
+        )
+
+    def test_find_max_deadline_on_operational_objective_children(self):
+        """Test find_max_deadline_on_operational_objective_children method on operational objective."""
+        self.assertEqual(
+            find_max_deadline_on_children(
+                self.oo_15,
+                {
+                    "pstsubaction": "planned_end_date",
+                    "subaction_link": "planned_end_date",
+                    "task": "due_date"
+                }
+            ),
+            datetime.date(datetime(2020, 10, 31))
+        )
+
+    def test_find_max_deadline_on_pst_action_children(self):
+        """Test find_max_deadline_on_pst_action_children method on pst action."""
+        self.assertEqual(
+            find_max_deadline_on_children(
+                self.a_16,
+                {
+                    "pstsubaction": "planned_end_date",
+                    "subaction_link": "planned_end_date",
+                    "task": "due_date"
+                }
+            ),
+            datetime.date(datetime(2020, 10, 31))
+        )
+
+    def test_find_max_deadline_on_pst_sub_action_children(self):
+        """Test find_max_deadline_on_pst_sub_action_children method on pst sub action."""
+        self.assertEqual(
+            find_max_deadline_on_children(self.sa_17, {"task": "due_date"}),
+            datetime.date(datetime(2020, 4, 30))
+        )
+
+    def test_find_brains_on_operational_objective_parents(self):
+        """Test find_brains_on_operational_objective_parents method on operational objective."""
+        self.assertEqual(
+            [containers_brains[0].Title for containers_brains in find_brains_on_parents(self.oo_15)],
+            [
+                "Etre une commune qui s'inscrit dans la lign\xc3\xa9e des accords de r\xc3\xa9ductions des gaz "
+                "\xc3\xa0 effet de serre afin d'assurer le d\xc3\xa9veloppement durable (OS.10)"
+            ]
+        )
+
+    def test_find_brains_on_pst_action_parents(self):
+        """Test find_brains_on_pst_action_parents method on pst action."""
+        self.assertEqual(
+            [containers_brains[0].Title for containers_brains in find_brains_on_parents(self.a_16)],
+            [
+                "R\xc3\xa9duire la consommation \xc3\xa9nerg\xc3\xa9tique des b\xc3\xa2timents communaux de 20% d'ici "
+                "2024 (OO.15)",
+                "Etre une commune qui s'inscrit dans la lign\xc3\xa9e des accords de r\xc3\xa9ductions des gaz "
+                "\xc3\xa0 effet de serre afin d'assurer le d\xc3\xa9veloppement durable (OS.10)"
+            ]
+        )
+
+    def test_find_brains_on_pst_sub_action_parents(self):
+        """Test find_brains_on_pst_sub_action_parents method on pst sub action."""
+        self.assertEqual(
+            [containers_brains[0].Title for containers_brains in find_brains_on_parents(self.sa_17)],
+            [
+                "R\xc3\xa9duire la consommation \xc3\xa9nerg\xc3\xa9tique de l'administration communale (A.16)",
+                "R\xc3\xa9duire la consommation \xc3\xa9nerg\xc3\xa9tique des b\xc3\xa2timents communaux de 20% d'ici "
+                "2024 (OO.15)",
+                "Etre une commune qui s'inscrit dans la lign\xc3\xa9e des accords de r\xc3\xa9ductions des gaz "
+                "\xc3\xa0 effet de serre afin d'assurer le d\xc3\xa9veloppement durable (OS.10)"
+            ]
+        )
+
+    def test_find_brains_on_task_parents(self):
+        """Test find_brains_on_task_parents method on task."""
+        self.assertEqual(
+            [containers_brains[0].Title for containers_brains in find_brains_on_parents(self.task)],
+            [
+                "R\xc3\xa9aliser un audit \xc3\xa9nerg\xc3\xa9tique du b\xc3\xa2timent (SA.17)",
+                "R\xc3\xa9duire la consommation \xc3\xa9nerg\xc3\xa9tique de l'administration communale (A.16)",
+                "R\xc3\xa9duire la consommation \xc3\xa9nerg\xc3\xa9tique des b\xc3\xa2timents communaux de 20% d'ici "
+                "2024 (OO.15)",
+                "Etre une commune qui s'inscrit dans la lign\xc3\xa9e des accords de r\xc3\xa9ductions des gaz "
+                "\xc3\xa0 effet de serre afin d'assurer le d\xc3\xa9veloppement durable (OS.10)"
+            ]
+        )
+
+    def test_find_deadlines_on_pst_action_parents(self):
+        """Test find_deadlines_on_pst_action_parents method on pst action."""
+        self.assertEqual(
+            find_deadlines_on_parents(self.a_16, {"operationalobjective": "planned_end_date"}),
+            [datetime.date(datetime(2024, 12, 31))]
+        )
+
+    def test_find_deadlines_on_pst_sub_action_parents(self):
+        """Test find_deadlines_on_pst_sub_action_parents method on pst sub action."""
+        self.assertEqual(
+            find_deadlines_on_parents(
+                self.sa_17,
+                {"pstaction": "planned_end_date", "operationalobjective": "planned_end_date"}
+            ),
+            [datetime.date(datetime(2024, 6, 30)), datetime.date(datetime(2024, 12, 31))]
+        )
+
+    def test_find_deadlines_on_task_parents(self):
+        """Test find_deadlines_on_task_parents method on task."""
+        self.assertEqual(
+            find_deadlines_on_parents(
+                self.task,
+                {
+                    "pstsubaction": "planned_end_date",
+                    "pstaction": "planned_end_date",
+                    "operationalobjective": "planned_end_date"
+                }
+            ),
+            [
+                datetime.date(datetime(2020, 6, 30)),
+                datetime.date(datetime(2024, 6, 30)),
+                datetime.date(datetime(2024, 12, 31))
+            ]
+        )
+
+    def test_find_max_deadline_on_pst_action_parents(self):
+        """Test find_max_deadline_on_pst_action_parents method on pst action."""
+        self.assertEqual(
+            find_max_deadline_on_parents(self.a_16, {"operationalobjective": "planned_end_date"}),
+            datetime.date(datetime(2024, 12, 31))
+        )
+
+    def test_find_max_deadline_on_pst_sub_action_parents(self):
+        """Test find_max_deadline_on_pst_sub_action_parents method on pst sub action."""
+        self.assertEqual(
+            find_max_deadline_on_parents(
+                self.sa_17,
+                {"pstaction": "planned_end_date", "operationalobjective": "planned_end_date"}
+            ),
+            datetime.date(datetime(2024, 12, 31))
+        )
+
+    def test_find_max_deadline_on_task_parents(self):
+        """Test find_max_deadline_on_task_parents method on pst task."""
+        self.assertEqual(
+            find_max_deadline_on_parents(
+                self.task,
+                {"pstaction": "planned_end_date", "operationalobjective": "planned_end_date"}
+            ),
+            datetime.date(datetime(2024, 12, 31))
+        )
