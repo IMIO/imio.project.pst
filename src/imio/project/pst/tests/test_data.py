c6de2e059259767bdb3925eb42ebe9c54becd20f
fngaha <franck.o.ngaha@gmail.com>
2021-06-09 15:07:54 +0200

    Fixed test : updated templates list PRJ-585

--- src/imio/project/pst/tests/test_data.py
+++ src/imio/project/pst/tests/test_data.py
@@ -18,6 +18,6 @@ class TestData(IntegrationTestCase):
                 'style', 'style_wo_nb', 'detail', 'detail-tasks', 'follow', 'follow-tasks', 'export', 'detail-all',
                 'detail-tasks-all', 'ddetail', 'ddetail-tasks', 'dfollow', 'dfollow-tasks', 'dexport', 'ddetail-all',
                 'ddetail-tasks-all', 'follow-all', 'follow-tasks-all', 'dfollow-all', 'dfollow-tasks-all', 'managers',
-                'dmanagers',
+                'dmanagers', 'editors', 'deditors',
             ]
         )
