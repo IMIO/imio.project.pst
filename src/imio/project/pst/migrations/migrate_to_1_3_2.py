8658475d390dd15a5239714c61c2594d56061e97
fngaha <franck.o.ngaha@gmail.com>
2021-06-09 14:47:09 +0200

    Preparing migration 1.3.2

---
+++ src/imio/project/pst/migrations/migrate_to_1_3_2.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from collective.documentgenerator.utils import update_oo_config
+from collective.messagesviewlet.utils import add_message
+from imio.helpers.content import create
+from imio.migrator.migrator import Migrator
+from imio.project.pst.data import get_main_templates
+from imio.project.pst.data import get_styles_templates
+from imio.project.pst.data import get_templates
+from plone import api
+
+logger = logging.getLogger('imio.project.pst')
+
+
+def update_templates():
+    """Add new templates"""
+    cids = create(get_styles_templates())
+    cids.update(create(get_main_templates(cids)))
+    cids.update(create(get_templates(cids)))
+
+
+class Migrate_To_1_3_2(Migrator):
+
+    def __init__(self, context):
+        Migrator.__init__(self, context)
+
+    def run(self):
+        # check if oo port must be changed
+        import ipdb; ipdb.set_trace()
+        update_oo_config()
+
+        self.runProfileSteps('imio.project.pst', steps=['imioprojectpst-override-templates'], profile='update',
+                             run_dependencies=False)
+
+        # upgrade all except 'imio.project.pst:default'. Needed with bin/upgrade-portals
+        # self.upgradeAll(omit=['imio.project.pst:default'])
+
+        # for prod in []:
+        #     mark_last_version(self.portal, product=prod)
+
+        # Reorder css and js
+        self.runProfileSteps('imio.project.pst', steps=['cssregistry', 'jsregistry'], run_dependencies=False)
+
+        # Add a new-version warning message in message config
+        self.add_new_version_message()
+
+        # Add new templates
+        update_templates()
+
+        # Display duration
+        self.finish()
+
+    def add_new_version_message(self):
+        if 'new-version' in self.portal['messages-config']:
+            api.content.delete(self.portal['messages-config']['new-version'])
+        add_message(
+            'new-version',
+            'Version 1.3.2',
+            u'<p>Vous êtes passés à la version d\'iA.PST 1.3.2 !</p>'
+            u'<p>La <a href="https://docs.imio.be/imio-doc/ia.pst/" target="_blank">'
+            u'documentation</a> a été mise à jour et comporte une nouvelle section sur les fonctionnalités</a>.</p>',
+            msg_type='warning',
+            can_hide=True,
+            req_roles=['Authenticated'],
+            activate=True
+        )
+
+    def add_new_dashboard_message(self):
+        if 'new-version' in self.portal['messages-config']:
+            api.content.delete(self.portal['messages-config']['new-version'])
+        if 'new-dashboard' not in self.portal['messages-config']:
+            add_message(
+                'new-dashboard',
+                'Nouveau modèle de tableau de bord',
+                u'<p>Votre instance a été mise à jour avec <a href="https://docs.imio.be/imio-doc/ia.pst/'
+                u'fonctionnalites/nouveau_modele_tableau_bord.html" target="_blank">une nouvelle fonctionnalité</a>.'
+                u'</p>',
+                msg_type='warning',
+                can_hide=True,
+                req_roles=['Authenticated'],
+                activate=True
+            )
+
+
+def migrate(context):
+    Migrate_To_1_3_2(context).run()
